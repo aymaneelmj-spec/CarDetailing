8f89e3ce4bb2a28776f726559dbdc8ab5a2b4394
aymaneelmj-spec <aymaneelmj@gmail.com>
2026-08-20 00:15:33 +0100

    update

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,19 +1,5 @@
-import heroMacro from './images/hero_macro.jpg';
-import porsche911 from './images/porsche_911_gt3.jpg';
-import mercedesG63 from './images/mercedes_g63.jpg';
-import ferrariF8 from './images/ferrari_f8.jpg';
-import rollsRoyce from './images/rolls_royce.jpg';
-import bmwM4 from './images/bmw_m4.jpg';
-import astonMartin from './images/aston_martin.jpg';
+import { embeddedImages } from './imagesData';
 
-export const vehicleImages = {
-  heroMacro,
-  porsche911,
-  mercedesG63,
-  ferrariF8,
-  rollsRoyce,
-  bmwM4,
-  astonMartin,
-};
+export const vehicleImages = embeddedImages;
 
 export default vehicleImages;
